efe77626f7f32da718f6a392ac7e4a89c6035002
Lokrdh <Lokrdh@192.168.56.1>
2023-07-13 13:09:17 +0530

    first commit

---
+++ src/test/java/sample/practiceTEST.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class practiceTEST {
+	@Test
+	
+
+	public void mtest()
+	{
+		System.out.println("hii");
+		// TODO Auto-generated method stub
+
+	}
+
+}
